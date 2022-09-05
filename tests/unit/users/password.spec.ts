6076673dc9aa3da4ac2796a0df6fc6fa0ea058b4
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-04 22:49:38 -0300

    feat tests/password

--- tests/unit/users/password.spec.ts
+++ tests/unit/users/password.spec.ts
@@ -1,4 +1,5 @@
 import Mail from "@ioc:Adonis/Addons/Mail";
+import Hash from "@ioc:Adonis/Core/Hash";
 import Database from "@ioc:Adonis/Lucid/Database";
 import { test } from "@japa/runner";
 import { UserFactory } from "Database/factories";
@@ -62,11 +63,44 @@ test.group("Password", (group) => {
   });
 
   // eslint-disable-next-line max-len
-  test("It should return 422 when required data is not provided or date is invalid", async ({
+  test("It should return 422 when data required for forget-password is not provided or data is invalid", async ({
     assert,
   }) => {
     const { body } = await supertest(BASE_URL)
       .post("/forgot-password")
+      .send({})
+      .expect(422);
+
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 422);
+  });
+
+  test("It should be able to reset password", async ({ assert }) => {
+    const user = await UserFactory.create();
+
+    const { token } = await user.related("tokens").create({ token: "token" });
+
+    await supertest(BASE_URL)
+      .post("/reset-password")
+      .send({
+        token,
+        password: "123456",
+      })
+      .expect(204);
+
+    await user.refresh();
+
+    const checkPassword = await Hash.verify(user.password, "123456");
+
+    assert.isTrue(checkPassword);
+  });
+
+  // eslint-disable-next-line max-len
+  test("It should return 422 when data required for reset-password is not provided or data is invalid", async ({
+    assert,
+  }) => {
+    const { body } = await supertest(BASE_URL)
+      .post("/reset-password")
       .send({})
       .expect(422);
 
