d1a993e522c21ee6b8b41324f0d5949fe19e6802
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-07 12:37:56 -0300

    feat tests/sessions

---
+++ tests/unit/users/sessions.spec.ts
@@ -0,0 +1,31 @@
+import { test } from "@japa/runner";
+// import Hash from "@ioc:Adonis/Core/Hash";
+import Database from "@ioc:Adonis/Lucid/Database";
+import { UserFactory } from "Database/factories";
+// import { DateTime, Duration } from "luxon";
+import supertest from "supertest";
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`;
+
+test.group("Sessions", (group) => {
+  test("It should authenticate an user", async ({ assert }) => {
+    const plainPassword = "test";
+
+    const { id, email } = await UserFactory.merge({
+      password: plainPassword,
+    }).create();
+
+    const { body } = await supertest(BASE_URL)
+      .post("/sessions")
+      .send({ email, password: plainPassword })
+      .expect(201);
+
+    assert.isDefined(body.user, "User undefined");
+    assert.equal(body.user.id, id);
+  });
+
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction();
+    return () => Database.rollbackGlobalTransaction();
+  });
+});
