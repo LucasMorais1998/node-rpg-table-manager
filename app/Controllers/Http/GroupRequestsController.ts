486535f05bad2b395ae559a8c78ccf2dd57fafdf
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-18 22:22:54 -0300

    feat Controllers/GroupRequestsController

--- app/Controllers/Http/GroupRequestsController.ts
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import GroupRequest from "App/Models/GroupRequest";
 
 export default class GroupRequestsController {
-  public async store({ request, response }: HttpContextContract) {
-    return response.created({});
+  public async store({ request, response, auth }: HttpContextContract) {
+    const groupId = request.param("groupId") as number;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const userId = auth.user!.id;
+
+    const groupRequest = await GroupRequest.create({ groupId, userId });
+    await groupRequest.refresh();
+
+    return response.created({ groupRequest });
   }
 }
