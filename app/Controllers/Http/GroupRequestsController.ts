68a5140d46347af7494133d60689153609fc81e9
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-18 23:09:12 -0300

    feat Controllers/GroupRequestsController

--- app/Controllers/Http/GroupRequestsController.ts
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import BadRequest from "App/Exceptions/BadRequestException";
+import Group from "App/Models/Group";
 import GroupRequest from "App/Models/GroupRequest";
 
 export default class GroupRequestsController {
@@ -15,6 +16,16 @@ export default class GroupRequestsController {
 
     if (existingGroupRequest)
       throw new BadRequest("group request already exists", 409);
+
+    const userAlreadyInGroup = await Group.query()
+      .whereHas("players", (query) => {
+        query.where("id", userId);
+      })
+      .andWhere("id", groupId)
+      .first();
+
+    if (userAlreadyInGroup)
+      throw new BadRequest("user is already in the group", 422);
 
     const groupRequest = await GroupRequest.create({ groupId, userId });
     await groupRequest.refresh();
