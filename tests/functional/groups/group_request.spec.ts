8326430ea62da0deee581899008e12f8372d2d4e
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-19 11:02:28 -0300

    feat tests/groups

--- tests/functional/groups/group_request.spec.ts
+++ tests/functional/groups/group_request.spec.ts
@@ -79,9 +79,26 @@ test.group("Group Requset", (group) => {
     const master = await UserFactory.create();
     const group = await GroupFactory.merge({ master: master.id }).create();
 
-    await supertest(BASE_URL)
+    const response = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({});
+
+    const groupRequest = response.body.groupRequest;
+
+    const { body } = await supertest(BASE_URL)
       .get(`/groups/${group.id}/requests?master=${master.id}`)
       .expect(200);
+
+    assert.exists(body.groupRequests, "GroupRequests undefined");
+    assert.equal(body.groupRequests.length, 1);
+    assert.equal(body.groupRequests[0].id, groupRequest.id);
+    assert.equal(body.groupRequests[0].userId, groupRequest.userId);
+    assert.equal(body.groupRequests[0].groupId, groupRequest.groupId);
+    assert.equal(body.groupRequests[0].status, groupRequest.status);
+    assert.equal(body.groupRequests[0].group.name, group.name);
+    assert.equal(body.groupRequests[0].user.username, user.username);
+    assert.equal(body.groupRequests[0].group.master, master.id);
   });
 
   group.setup(async () => {
