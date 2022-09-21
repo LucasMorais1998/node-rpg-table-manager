a81c9903db9c7f24e25c85797f92bee72445a202
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-21 14:51:25 -0300

    feat tests/groups

--- tests/functional/groups/group.spec.ts
+++ tests/functional/groups/group.spec.ts
@@ -1,7 +1,7 @@
 import Database from "@ioc:Adonis/Lucid/Database";
 import { test } from "@japa/runner";
 import User from "App/Models/User";
-import { UserFactory } from "Database/factories";
+import { GroupFactory, UserFactory } from "Database/factories";
 import supertest from "supertest";
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`;
@@ -49,6 +49,44 @@ test.group("Group", (group) => {
 
     assert.equal(body.code, "BAD_REQUEST");
     assert.equal(body.status, 422);
+  });
+
+  test("It should update a group", async ({ assert }) => {
+    const master = await UserFactory.create();
+    const group = await GroupFactory.merge({ master: master.id }).create();
+
+    const payload = {
+      name: "test",
+      description: "test",
+      schedule: "test",
+      location: "test",
+      chronic: "test",
+    };
+
+    const { body } = await supertest(BASE_URL)
+      .patch(`/groups/${group.id}`)
+      .send(payload)
+      .expect(200);
+
+    assert.exists(body.group, "Group undefined");
+    assert.equal(body.group.name, payload.name);
+    assert.equal(body.group.description, payload.description);
+    assert.equal(body.group.schedule, payload.schedule);
+    assert.equal(body.group.location, payload.location);
+    assert.equal(body.group.chronic, payload.chronic);
+  });
+
+  // eslint-disable-next-line max-len
+  test("It should return 404 when providing an unexisting group for update", async ({
+    assert,
+  }) => {
+    const response = await supertest(BASE_URL)
+      .patch("/groups/1")
+      .send({})
+      .expect(404);
+
+    assert.equal(response.body.code, "BAD_REQUEST");
+    assert.equal(response.body.status, 404);
   });
 
   group.setup(async () => {
